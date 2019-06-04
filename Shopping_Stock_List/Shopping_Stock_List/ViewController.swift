3e21339abf95809f089333e232ff9c025a3557fc
光岡駿 <mitsuokashun@mitsuokashunnoMacBook-puro.local>
2019-06-04 12:15:15 +0900

    Shopping_Stock_List version1.0.0

---
+++ Shopping_Stock_List/Shopping_Stock_List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping_Stock_List
+//
+//  Created by 光岡駿 on 2019/06/04.
+//  Copyright © 2019 光岡駿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Shopping_Stock_List/Shopping_Stock_ListTests/Shopping_Stock_ListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Shopping_Stock_ListTests.swift
+//  Shopping_Stock_ListTests
+//
+//  Created by 光岡駿 on 2019/06/04.
+//  Copyright © 2019 光岡駿. All rights reserved.
+//
+
+import XCTest
+@testable import Shopping_Stock_List
+
+class Shopping_Stock_ListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

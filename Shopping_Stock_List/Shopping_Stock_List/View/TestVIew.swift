71b191b6408ae67eb16f547c936f82f0ac6e97bc
光岡駿 <mitsuokashun@mitsuokashunnoMacBook-puro.local>
2019-08-17 23:56:28 +0900

    ShoppingStockList

---
+++ Shopping_Stock_List/Shopping_Stock_List/View/TestVIew.swift
@@ -0,0 +1,30 @@
+//
+//  TestVIew.swift
+//  Shopping_Stock_List
+//
+//  Created by 光岡駿 on 2019/06/06.
+//  Copyright © 2019 光岡駿. All rights reserved.
+//
+
+import UIKit
+
+class TestVIew: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Shopping_Stock_List/Shopping_Stock_List/ViewProperties.swift
@@ -0,0 +1,9 @@
+//
+//  ViewProperties.swift
+//  Shopping_Stock_List
+//
+//  Created by 光岡駿 on 2019/06/06.
+//  Copyright © 2019 光岡駿. All rights reserved.
+//
+
+import Foundation
